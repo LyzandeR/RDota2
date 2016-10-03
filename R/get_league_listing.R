950e64bbd9f462ab381ab7edb11a7ad231d4affa
Teo <teoboot2007@hotmail.com>
2016-10-03 01:13:09 +0100

    improved get_league_listing, added register_key and added documentation

--- R/get_league_listing.R
+++ R/get_league_listing.R
@@ -1,21 +1,62 @@
+#' Information about DotaTV-supported leagues
+#'
+#' \code{add_css_caption} will return a data.frame with information about DotaTV-supported leagues.
+#'
+#' A list will be returned that contains three elements. The content, the url and the response
+#' received from the api.
+#'
+#' The content is probably the most useful part for the user since it is a data.frame containing the
+#' information about the DotaTV-supported leagues. It consists of the five following columns:
+#' \itemize{
+#'   \item \strong{leagues:} The leagues supported in-game via DotaTV.
+#'   \item \strong{name:} The league name.
+#'   \item \strong{leagueid:} The ID of the league (unique).
+#'   \item \strong{description:} A description containing information about the league.
+#'   \item \strong{tournament_url:} The website of the link.
+#' }
+#'
+#' @param key The api key obtained from Steam. If you don't have one please visit
+#' \url{https://steamcommunity.com/dev} in order to do so. For instructions on the correct way
+#' to use this key please visit \url{https://github.com/LyzandeR/RDota2} and check the readme file.
+#' You can also see the examples. A key can be made available to all the functions by using
+#' \code{register_key}. The key argument in individual functions should only be used in case the
+#' user needs to work with multiple keys.
+#'
+#' @param language The ISO639-1 language code for returning all the information in the corresponding
+#' language. If the language is not supported, english will be returned. For a complete list of the
+#' ISO639-1 language codes please visit \url{https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes}.
+#'
+#' @return A dota_api object containing the elements described in the details.
+#'
+#' @examples
+#' \dontrun{
+#' get_league_listing(language = 'en', key = NULL)
+#' get_league_listing(language = 'en', key = 'xxxxxxxxxxx')
+#' }
+#'
+#' @export
 get_league_listing <- function(language = 'en', key = NULL) {
 
  #if key is null look in the environment variables
  if (is.null(key)) {
-  key <- Sys.getenv('RDota_KEY')
+  key <- get_key()
 
   #if key is blank space then stop i.e. environment variable has not be set.
-  if (!nzchar(key)) {
-   stop(strwrap('You have not provided a key OR the RDota_KEY environent variable could not
-               be loaded from your home path. If you do not have a key you can obtain one by
-               visiting https://steamcommunity.com/dev. Check the details in ?get_league_listing.',
+  if (!nzchar(key) | is.null(key)) {
+   stop(strwrap('The function cannot find an API key. Please register a key by using
+                the RDota2::register_key function. If you do not have a key you can
+                obtain one by visiting https://steamcommunity.com/dev.',
                 width = 1e10))
   }
  }
 
+ #set a user agent
+ ua <- user_agent("http://github.com/lyzander/RDota2")
+
  resp <- GET('http://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v1/',
-             query = list(key = key,
-                          language = language))
+             query = list(key = key, language = language), ua)
+
+ #get url
  url <- strsplit(resp$url, '\\?')[[1]][1]
 
  #check for code status. Any http errors will be converted to something meaningful.
--- R/key_funcs.R
+++ R/key_funcs.R
@@ -2,22 +2,71 @@
 .RdotaEnv <- new.env(parent = emptyenv())
 .RdotaEnv$key <- NULL
 
-# Set token to environment
-register_key <- function(value) {
+# Set key to environment
+set_key <- function(value) {
  .RdotaEnv$key <- value
- return(value)
+ message('Key set successfully')
+ invisible(value)
 }
 
-# Get token from environment
+# Get key from environment
 get_key <- function() {
  .RdotaEnv$key
 }
 
+#delete key from environment
 delete_key <- function() {
  rm('key', envir = .RdotaEnv)
 }
 
+#' This function makes the key accessible to all functions.
+#'
+#' \code{register_key} will allow the user to set, get (retrieve) or delete the key.
+#'
+#' There are three operations that can be selected in this function:
+#' \itemize{
+#'   \item \strong{set_key} This operation sets the key and at the same time makes it available to
+#'                          all the other functions. The \code{key} argument in the api calls
+#'                          (i.e. functions) should not be used after setting the key with
+#'                          \code{register_key}. It is good practice to store the key in an
+#'                          environment variable in .Renviron and then use \code{Sys.getenv()} to
+#'                          retrieve it. This technique is described in detail in
+#'                          \url{https://github.com/LyzandeR/RDota2} in the readme file.
+#'   \item \strong{get_key} Returns the current key.
+#'   \item \strong{delete_key} Deletes the key.
+#' }
+#'
+#' @param operation Which operation to perform. Check the details.
+#'
+#' @param value The steam key to use. It is used only in the \code{set_key} operation. If you do not
+#'        have a key, you can get one at \url{https://steamcommunity.com/dev}.
+#'
+#' @return A dota_api object containing the elements described in the details.
+#'
+#' @examples
+#' \dontrun{
+#' register_key(operation = 'set_key', value = Sys.getenv('RDota_KEY'))
+#' register_key(operation = 'set_key', value = 'xxxxxxxxxxxxx')
+#' register_key(operation = 'get_key')
+#' register_key(operation = 'delete_key')
+#' }
+#'
+#' @export
+register_key <- function(operation = c('set_key', 'get_key', 'delete_key'), value = NULL) {
 
+ #make sure right operation was provided
+ operation <- match.arg(operation)
 
+ if (operation == 'set_key') {
+  set_key(value)
+ } else if (operation == 'get_key') {
+  get_key()
+ } else if (operation == 'delete_key') {
+  delete_key()
+ }
+
+ invisible(value)
+
+}
 
 
